a8c866330388ec8ee447b490a51b6deb41bb363a
py1150 <berndgaller@gmail.com>
2020-10-10 15:46:08 +0200

    data connected

--- starter_code/config.py
+++ starter_code/config.py
@@ -10,4 +10,12 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+#SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+#SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+
+dbname = 'project1'
+db_user = 'udacity'
+db_pw = 'pwabc'
+SQLALCHEMY_DATABASE_URI = f'postgresql://{db_user}:{db_pw}@localhost:5432/{dbname}'
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
--- starter_code/create_db.py
+++ starter_code/create_db.py
@@ -1,120 +1,220 @@
-#----------------------------------------------------------------------------#
-# Imports
-#----------------------------------------------------------------------------#
+"""
+Start Data
+"""
 
-import json
-import dateutil.parser
-import babel
-from flask import Flask, render_template, request, Response, flash, redirect, url_for
-from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
-import logging
-from logging import Formatter, FileHandler
-from flask_wtf import Form
-from forms import *
+#from sqlalchemy import create_engine, MetaData, Table, Column
+from sqlalchemy import *
+from sqlalchemy_utils import database_exists, create_database
+#import sqlalchemy_utils as sqlut
+from sqlalchemy.ext.declarative import declarative_base
 
 import pdb
-#----------------------------------------------------------------------------#
-# App Config.
-#----------------------------------------------------------------------------#
-
-app = Flask(__name__)
-moment = Moment(app)
-app.config.from_object('config')
-db = SQLAlchemy(app)
-
-# TODO: connect to a local postgresql database
 
 
-#----------------------------------------------------------------------------#
-# Models.
-#----------------------------------------------------------------------------#
+def create_db(db_url):
+    # create database if it does not exist
+    engine = create_engine(db_url, echo=True)
+    """
+    if not database_exists(engine.url):
+        create_database(engine.url)
+        print(f'Database {engine.db_url} created')
+    else:
+        print(f'Database {engine.db_url} exists')
+    """    
+    #else:
+    #    engine.connect()
+    #print(f'Database {engine.db_url} created')
 
-class Venue(db.Model):
-    __tablename__ = 'Venue'
+    # create tables if they do not exist
+    #pdb.set_trace()
+    if engine.dialect.has_table(engine, 'Venue')==False: 
+        metadata = MetaData(engine)
+        # Create schema
+        venue = Table(Venue, metadata,\
+            Column('id', Integer, primary_key=True, nullable=False),\
+            Column('name', String),\
+            Column('city', String),\
+            Column('state', String),\
+            Column('address', String),\
+            Column('phone', String),\
+            Column('image_link', String),\
+            Column('facebook_link', String),\
+            Column('website', String),\
+            Column('genres', String),\
+            Column('seeking_talent', Boolean),\
+            Column('seeking_description', String),\
+            Column('past_shows_count', Integer),\
+            Column('upcoming_shows_count', Integer))
+        # create
+        metadata.create_all()
+        print(f'Database tables created')
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    seeking_talent = db.Column(db.Boolean)
+    return engine
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-class Artist(db.Model):
-    __tablename__ = 'Artist'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
-
-class Show(db.Model):
-    __tablename__ = 'Show'
-
-    id = db.Column(db.Integer, primary_key=True)
-    start_time = db.Column(db.DateTime, nullable=False)
-    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
-    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
-
-# o
-db.create_all()
-
-
-#----------------------------------------------------------------------------#
-# Filters.
-#----------------------------------------------------------------------------#
-
-def format_datetime(value, format='medium'):
-  date = dateutil.parser.parse(value)
-  if format == 'full':
-      format="EEEE MMMM, d, y 'at' h:mma"
-  elif format == 'medium':
-      format="EE MM, dd, y h:mma"
-  return babel.dates.format_datetime(date, format)
-
-app.jinja_env.filters['datetime'] = format_datetime
-
-#----------------------------------------------------------------------------#
-# Controllers.
-#----------------------------------------------------------------------------#
-
-@app.route('/')
-def index():
-  return render_template('pages/home.html')
+def create_table_venue(engine):
+    if engine.dialect.has_table(engine, 'Venue')==False: 
+        #metadata = MetaData(engine)
+        metadata = MetaData()
+        # Create schema
+        venue = Table('Venue', metadata,\
+            Column('id', Integer, primary_key=True, nullable=False),\
+            Column('name', String),\
+            Column('city', String),\
+            Column('state', String),\
+            Column('address', String),\
+            Column('phone', String),\
+            Column('image_link', String),\
+            Column('facebook_link', String),\
+            Column('website', String),\
+            Column('genres', String),\
+            Column('seeking_talent', Boolean),\
+            Column('seeking_description', String),\
+            Column('past_shows_count', Integer),\
+            Column('upcoming_shows_count', Integer))
+            
+        # create
+        metadata.create_all(engine)
+        print('Table Venue created')
+    else:
+        metadata = MetaData(bind=engine, reflect=True)
+        venue = metadata.tables['Venue']
+        print('Table Venue already exists')
+    return venue
 
 
-#  Venues
-#  ----------------------------------------------------------------
+def get_venue_data():
+
+    venue_data = [{
+        "id": 1,
+        "name": "The Musical Hop",
+        "city": "San Francisco",
+        "state": "CA",
+        "address":"1015 Folsom Street",
+        "phone":"123-123-1234",
+        "image_link":"https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        "facebook_link":"https://www.facebook.com/TheMusicalHop",
+        "website":"https://www.themusicalhop.com",
+        "genres":"Jazz, Reggae, Swing, Classical, Folk",
+        "seeking_talent":True,
+        "seeking_description":"We are on the lookout for a local artist to play every two weeks. Please call us.",
+        "past_shows_count":1,
+        "upcoming_shows_count":0,
+       },{ 
+        "id": 2,
+        "name": "The Dueling Pianos Bar",
+        "city": "New York",
+        "state": "NY",
+        "address":"335 Delancey Street",
+        "phone":"914-003-1132",
+        "image_link":"https://images.unsplash.com/photo-1497032205916-ac775f0649ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+        "facebook_link":"https://www.facebook.com/theduelingpianos",
+        "website":"https://www.theduelingpianos.com",
+        "genres":"[Classical, R&B, Hip-Hop]",
+        "seeking_talent":False,
+        "seeking_description":"",
+        "past_shows_count":0,
+        "upcoming_shows_count":0,
+       }
+       ,{
+        "id": 3,
+        "name": "Park Square Live Music & Coffee",
+        "city": "San Francisco",
+        "state": "CA",
+        "address":"34 Whiskey Moore Ave",
+        "phone":"415-000-1234",
+        "image_link":"https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
+        "facebook_link":"https://www.facebook.com/ParkSquareLiveMusicAndCoffee",
+        "website":"https://www.parksquarelivemusicandcoffee.com",
+        "genres":"Rock n Roll, Jazz, Classical, Folk",
+        "seeking_talent":False,
+        "seeking_description":"",
+        "past_shows_count":1,
+        "upcoming_shows_count":1,
+    }]
+
+    return venue_data
 
 
-@app.route('/venues')
+if __name__=='__main__':
+
+    # input parameters
+    # ----------------
+    # database
+    dbname = 'project1'
+    db_user = 'udacity'
+    db_pw = 'pwabc'
+    
+
+    
+    # fill db
+    # ----------------
+
+    # get db_url
+    db_url = f'postgresql://{db_user}:{db_pw}@localhost:5432/{dbname}'
+    print(f'db_url: {db_url}')
+
+    # create database if it does not exist
+    #engine = create_db(db_url=db_url)
+    # create engine
+    engine = create_engine(db_url, echo=True)
+
+    # create venue table in database
+    venue = create_table_venue(engine=engine)
+
+    # get data
+    venue_data = get_venue_data()
+
+    # insert records
+    with engine.connect() as conn:
+        ins = venue.insert().values(venue_data)
+        result = conn.execute(ins)
+        #conn.commit()
+
+
+
+
+"""
+insert = Venue.insert().values(venue_data)
+
+result = conn.execute(ins)
+
+
+pdb.set_trace()
+"""
+
+"""
+#-----------------------------------------------------------
+#-----------------------------------------------------------
+# Input Data 
+#-----------------------------------------------------------
+#-----------------------------------------------------------
+# venues
+# -------
+
+
+venue_data = [{
+        "city": "San Francisco",
+        "state": "CA",
+        "venues": [{
+        "id": 1,
+        "name": "The Musical Hop",
+        "num_upcoming_shows": 0,
+        }, {
+        "id": 3,
+        "name": "Park Square Live Music & Coffee",
+        "num_upcoming_shows": 1,
+        }]
+    }, {
+        "city": "New York",
+        "state": "NY",
+        "venues": [{
+        "id": 2,
+        "name": "The Dueling Pianos Bar",
+        "num_upcoming_shows": 0,
+        }]
+    }]
+
 def venues():
-
-  # TODO: replace with real venues data.
-  #       num_shows should be aggregated based on number of upcoming shows per venue.
-  data = Venue.query.all()
-  return render_template('pages/venues.html', areas=data);
-
-""" orig
-@app.route('/venues')
-def venues():
-
   # TODO: replace with real venues data.
   #       num_shows should be aggregated based on number of upcoming shows per venue.
   data=[{
@@ -138,9 +238,6 @@ def venues():
       "num_upcoming_shows": 0,
     }]
   }]
-  return render_template('pages/venues.html', areas=data);
-"""
-
 
 
 @app.route('/venues/search', methods=['POST'])
@@ -156,25 +253,11 @@ def search_venues():
       "num_upcoming_shows": 0,
     }]
   }
-  return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
 
 
 
-@app.route('/venues/<int:venue_id>')
-def show_venue(venue_id):
-  # shows the venue page with the given venue_id
-  # TODO: replace with real venue data from the venues table, using venue_id
-  
-  #data = list(filter(lambda d: d['id'] == venue_id, [data1, data2, data3]))[0]
-
-  query = Venue.query.filter(Venue.id==venue_id)
-  #data = query.all()
-  data = query.first()
-  return render_template('pages/show_venue.html', venue=data)
 
 
-
-"""
 @app.route('/venues/<int:venue_id>')
 def show_venue(venue_id):
   # shows the venue page with the given venue_id
@@ -256,39 +339,14 @@ def show_venue(venue_id):
     "past_shows_count": 1,
     "upcoming_shows_count": 1,
   }
-  data = list(filter(lambda d: d['id'] == venue_id, [data1, data2, data3]))[0]
-  return render_template('pages/show_venue.html', venue=data)
-"""
 
 
-#  Create Venue
-#  ----------------------------------------------------------------
 
-@app.route('/venues/create', methods=['GET'])
-def create_venue_form():
-  form = VenueForm()
-  return render_template('forms/new_venue.html', form=form)
 
-@app.route('/venues/create', methods=['POST'])
-def create_venue_submission():
-  # TODO: insert form data as a new Venue record in the db, instead
-  # TODO: modify data to be the data object returned from db insertion
+  # artists
+  # ---------
 
-  # on successful db insert, flash success
-  flash('Venue ' + request.form['name'] + ' was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Venue ' + data.name + ' could not be listed.')
-  # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
-  return render_template('pages/home.html')
 
-@app.route('/venues/<venue_id>', methods=['DELETE'])
-def delete_venue(venue_id):
-  # TODO: Complete this endpoint for taking a venue_id, and using
-  # SQLAlchemy ORM to delete a record. Handle cases where the session commit could fail.
-
-  # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
-  # clicking that button delete it from the db then redirect the user to the homepage
-  return None
 
 #  Artists
 #  ----------------------------------------------------------------
@@ -397,8 +455,9 @@ def show_artist(artist_id):
     "past_shows_count": 0,
     "upcoming_shows_count": 3,
   }
-  data = list(filter(lambda d: d['id'] == artist_id, [data1, data2, data3]))[0]
-  return render_template('pages/show_artist.html', artist=data)
+
+
+
 
 #  Update
 #  ----------------------------------------------------------------
@@ -445,34 +504,7 @@ def edit_venue(venue_id):
     "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
     "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
   }
-  # TODO: populate form with values from venue with ID <venue_id>
-  return render_template('forms/edit_venue.html', form=form, venue=venue)
 
-@app.route('/venues/<int:venue_id>/edit', methods=['POST'])
-def edit_venue_submission(venue_id):
-  # TODO: take values from the form submitted, and update existing
-  # venue record with ID <venue_id> using the new attributes
-  return redirect(url_for('show_venue', venue_id=venue_id))
-
-#  Create Artist
-#  ----------------------------------------------------------------
-
-@app.route('/artists/create', methods=['GET'])
-def create_artist_form():
-  form = ArtistForm()
-  return render_template('forms/new_artist.html', form=form)
-
-@app.route('/artists/create', methods=['POST'])
-def create_artist_submission():
-  # called upon submitting the new artist listing form
-  # TODO: insert form data as a new Venue record in the db, instead
-  # TODO: modify data to be the data object returned from db insertion
-
-  # on successful db insert, flash success
-  flash('Artist ' + request.form['name'] + ' was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
-  return render_template('pages/home.html')
 
 
 #  Shows
@@ -519,56 +551,37 @@ def shows():
     "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
     "start_time": "2035-04-15T20:00:00.000Z"
   }]
-  return render_template('pages/shows.html', shows=data)
-
-@app.route('/shows/create')
-def create_shows():
-  # renders form. do not touch.
-  form = ShowForm()
-  return render_template('forms/new_show.html', form=form)
-
-@app.route('/shows/create', methods=['POST'])
-def create_show_submission():
-  # called to create new shows in the db, upon submitting new show listing form
-  # TODO: insert form data as a new Show record in the db, instead
-
-  # on successful db insert, flash success
-  flash('Show was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Show could not be listed.')
-  # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
-  return render_template('pages/home.html')
-
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template('errors/404.html'), 404
-
-@app.errorhandler(500)
-def server_error(error):
-    return render_template('errors/500.html'), 500
 
 
-if not app.debug:
-    file_handler = FileHandler('error.log')
-    file_handler.setFormatter(
-        Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
-    )
-    app.logger.setLevel(logging.INFO)
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.info('errors')
 
-#----------------------------------------------------------------------------#
-# Launch.
-#----------------------------------------------------------------------------#
 
-# Default port:
-if __name__ == '__main__':
-    app.run()
+  import psycopg2
 
-# Or specify port manually:
-'''
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-'''
+connection = psycopg2.connect('dbname=example')
+
+cursor = connection.cursor()
+
+cursor.execute('DROP TABLE IF EXISTS table2;')
+
+cursor.execute('''
+  CREATE TABLE table2 (
+    id INTEGER PRIMARY KEY,
+    completed BOOLEAN NOT NULL DEFAULT False
+  );
+''')
+
+cursor.execute('INSERT INTO table2 (id, completed) VALUES (%s, %s);', (1, True))
+
+SQL = 'INSERT INTO table2 (id, completed) VALUES (%(id)s, %(completed)s);'
+
+data = {
+  'id': 2,
+  'completed': False
+}
+cursor.execute(SQL, data)
+
+connection.commit()
+
+connection.close()
+cursor.close()
+"""
